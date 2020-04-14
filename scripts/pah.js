acdf14299227c7def4e7e7f5ed8f1b6ba25d9c48
Brenda Campbell <brenda.campbell@internal.salesforce.com>
2020-04-14 11:38:43 +0100

    version3

--- scripts/pah.js
+++ scripts/pah.js
@@ -9,7 +9,7 @@ exports.retrieve = function retrieve(callback) {
    var client = new Client();
    var argsHeader = {};
    var responseData = {};
-   var tokenResponseData = {};
+   var pushResponseData = {};
    
    var URL = process.env.AUTH_ENDPOINT;
    var args = {
@@ -18,11 +18,50 @@ exports.retrieve = function retrieve(callback) {
    };
 
    var argsHeader = JSON.stringify(args.headers); 
-   client.post(URL, args, function (data, response) {	    		
-		console.log(data);		
-		callback(data);		
+   client.post(URL, args, function (data, response) {	
+		responseData = data;
+		console.log(responseData);
+	
+		var accessToken = responseData['access_token'];
+		
+		console.log(accessToken);
+
+		var PUSHURL = "https://mcfqgk9bc35yxj6gw9y07g8wrbbq.rest.marketingcloudapis.com/push/v1/messageContact/NzoxMTQ6MA/send";
+		
+		var bearerToken = "Bearer " + accessToken;		
+		var pushArgs = {
+			data: getPushBodySection(),
+				headers: { "Authorization": bearerToken,
+				"content-type": "application/json",
+			}
+		};
+		
+		client.post(PUSHURL, pushArgs, function (data, response) {
+			pushResponseData = data;
+			if (response.statusCode == 200) {
+				console.log("Success sending push");
+			} 
+
+			console.log(pushResponseData);
+			callback({'AuthHeader': argsHeader, 'AuthBody': getBodySection(),'BearerToken' : bearerToken, 'PushArgs': JSON.stringify(pushArgs.headers),'SubscribersPassed': JSON.stringify(pushArgs.data), 'TokenID' : JSON.stringify(pushResponseData)});
+		});
+			
 	});		
   
+}
+
+function getPushBodySection() {
+	var data = JSON.stringify({
+		"subscriberKeys": [
+			  "e25ef4e8-82e4-4b98-96cf-5149c110cfe7"
+		  ],
+		  "Override": true,
+		  "MessageText": "Test trigger send from Heroku app for GI",
+		  "title": "Missing pet - Test from Brenda",
+		  "subtitle": "Lost Pet Alert via Heroku to Andy",
+		  "Badge": "+1"
+		});
+	return data;
 }
 
 function getBodySection() {
